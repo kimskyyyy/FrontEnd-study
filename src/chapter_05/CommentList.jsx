10c0235260925246568aa00f7c019edbe47c7b2b
kimskyyyy <hhhhh823@naver.com>
2023-05-10 00:28:34 +0900

    데이터 동적으로 받아오는 comment 컴포넌트

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import Comment from "./Comment";
 
+// 데이터를 별도의 객체로 분리해서 동적으로 받아온 데이터를표시할 수 있는 구조로 만들기
+
+// map()함수를 써서 각 댓글 객체에 대해서 Comment 컴포넌트를 리턴하도록 코드 작성
 function CommentList(props) {
     return (
         <div>
-            <Comment name={"sky"} comment={"안녕!!!!!!!!!"} />
-            <Comment name={"dol"} comment={"애옹!!!!!!!!!"} />
+             {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                );
+            })}
         </div>
     );
 }
+
+// comments 배열에 댓글 데이터를 담고 있는 객체를 넣어줌
+const comments = [
+    {
+        name: "sky",
+        comment: "안녕!!!!!!!!.",
+    },
+    {
+        name: "돌이",
+        comment: "애옹~!",
+    },
+    {
+        name: "솔이",
+        comment: "냐",
+    },
+];
+
 
 export default CommentList;
